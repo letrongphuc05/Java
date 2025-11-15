56b783911e2e52b5ba2fee5e4751c6feaa48f2a2
caswlling <caswllinggg@gmail.com>
2025-11-16 02:28:54 +0700

    Tân update trang login, register và home

--- CarRental/src/main/java/config/SecurityConfig.java
+++ CarRental/src/main/java/config/SecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class SecurityConfig {
-    @Autowireda
+    @Autowired
     private CustomLoginSuccessHandler customLoginSuccessHandler;
 
     @Bean
